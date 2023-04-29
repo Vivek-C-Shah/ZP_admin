efe5744048974b847628e3b3e96a1e50cda94639
Vivek Shah <94179705+Vivek-C-Shah@users.noreply.github.com>
2023-04-30 00:34:35 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,12 @@ const Header = () => {
 	return(
 		      <footer className="footer">
             <div className="d-sm-flex justify-content-center">
-              <span className="float-none text-center d-block mt-1 mt-sm-0 text-center space-x-2">Copyright © <a href={"https://www.linkedin.com/in/vivek-shah-b65507231/"} target="_blank" rel="noopener noreferrer" className="round-link">
+              <span className="float-none text-center d-block mt-1 mt-sm-0 text-center space-x-2">Created by <a href={"https://www.linkedin.com/in/vivek-shah-b65507231/"} target="_blank" rel="noopener noreferrer" className="round-link">
 								Vivek
 								</a> and <a> </a>
                 <a href={"https://www.linkedin.com/in/vrajshah113"} target="_blank" rel="noopener noreferrer">
 								 Vraj
-								</a> 2023. All rights reserved.</span>
+								</a>.Copyright ©  2023. All rights reserved.</span>
             </div>
           </footer>
 		)
